e8ca6efe20459460164fccee2de137b944014832
CSHuy1611 <he186445caosyhuy@gmail.com>
2025-11-19 13:47:33 +0700

    add new seedData

--- backend/FashionStoreSystem/FashionStore.WebAPI/Program.cs
+++ backend/FashionStoreSystem/FashionStore.WebAPI/Program.cs
@@ -1,3 +1,6 @@
+﻿using FashionStore.Data.Models;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -44,6 +47,18 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseSqlServer(connectionString,
         b => b.MigrationsAssembly("FashionStore.Data")));
 
+// Phải thêm Identity trước khi gọi SeedData vì SeedData cần UserManager và RoleManager
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+})
+.AddEntityFrameworkStores<ApplicationDBContext>()
+.AddDefaultTokenProviders();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -58,6 +73,21 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        // Gọi hàm Seed từ class tĩnh SeedData
+        await SeedData.Seed(services);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Find the error when run the seed data.");
+    }
 }
 
 // Configure the HTTP request pipeline.
