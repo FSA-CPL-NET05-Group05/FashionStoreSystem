76ba550cc0dba7af282b57bd5a6186b00605084f
hoangdt04 <maivanhoang112k4@gmail.com>
2025-11-24 09:13:08 +0700

    resolve conflict

--- frontend/fashion-store/src/app/app.routes.ts
+++ frontend/fashion-store/src/app/app.routes.ts
@@ -3,38 +3,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { AdminDashboardComponent } from './pages/admin/admin-dashboard/admin-dashboard.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
   { path: 'home', component: HomeComponent },
-
-  {
-    path: 'admin',
-    loadComponent: () =>
-      import('./pages/admin/admin.component').then((m) => m.AdminComponent),
-    children: [
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-
-      {
-        path: 'dashboard',
-        loadComponent: () =>
-          import(
-            './pages/admin/admin-dashboard/admin-dashboard.component'
-          ).then((m) => m.AdminDashboardComponent),
-      },
-      {
-        path: 'orders',
-        loadComponent: () =>
-          import('./pages/admin/admin-orders/admin-orders.component').then(
-            (m) => m.AdminOrdersComponent
-          ),
-      },
-      {
-        path: 'products',
-        loadComponent: () =>
-          import('./pages/admin/admin-products/admin-products.component').then(
-            (m) => m.AdminProductsComponent
-          ),
-      },
-    ],
-  },
+  { path: 'admin', component: AdminComponent },
+  { path: 'dashboard', component: AdminDashboardComponent },
 ];
