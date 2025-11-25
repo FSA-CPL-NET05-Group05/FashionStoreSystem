eebeac27bdd5a760fa079b4f4f34c20b41c70e3a
he186574tongvietduyhung <he186574tongvietduyhung@gmail.com>
2025-11-25 10:52:01 +0700

    Add API Category

---
+++ backend/FashionStoreSystem/FashionStore.Business/Service/Customer.Service/CategoryService.cs
@@ -0,0 +1,33 @@
+﻿using FashionStore.Business.Dtos.Dtos.Customer;
+using FashionStore.Business.Interfaces.Interfaces.Customer;
+using FashionStore.Data.Interfaces.Interfaces.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FashionStore.Business.Service.Customer.Service
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<List<CategoryDTO>> GetAllCategoriesAsync()
+        {
+            var categories = await _categoryRepository.GetAllCategoriesAsync();
+
+            return categories.Select(c => new CategoryDTO
+            {
+                Id = c.Id,
+                Name = c.Name
+            }).ToList();
+        }
+    }
+
+}
